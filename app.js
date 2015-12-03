0124b963969089babeb5f5deea072a3d18ddc959
Markham Shofner <markham.shofner@gmail.com>
2015-12-03 15:58:18 -0500

    draft model for location with oojs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 var locationsController = require("./controllers/locationsController");
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", function(req, res){
   res.render("index.html");
--- public/js/models/location.js
+++ public/js/models/location.js
@@ -1,10 +1,10 @@
 var Location = function(info){
-  this.name = name;
-  this.lat = lat;
-  this.long = long;
-  this.type = type;
-  this.score = score;
-  this.id = info.id;
+  this.name = info.name;
+  // this.lat = info.lat;
+  // this.long = info.long;
+  // this.type = info.type;
+  // this.score = info.score;
+  // this.id = info.id;
 };
 
 // module.exports = Location;
