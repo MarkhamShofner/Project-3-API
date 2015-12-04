5976fd862c9cca1b712a07d187ff1ab2f32168f9
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-04 14:51:27 -0500

    models built, pizza removed from app.js

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var app = express();
 var path = require("path");
 var bodyParser = require("body-parser");
 var locationsController = require("./controllers/locationsController");
-var helperPizza = require("./helpers/first");
 
-app.set("view engine", "hbs");
+// app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -17,5 +16,4 @@ app.get("/locations", locationsController.index);
 
 app.listen(3000, function(){
   console.log("app listening at http://localhost:3000/");
-  helperPizza.helper1();
 });
--- public/js/models/location.js
+++ public/js/models/location.js
@@ -1,10 +1,24 @@
 var Location = function(info){
   this.name = info.name;
-  // this.lat = info.lat;
-  // this.long = info.long;
-  // this.type = info.type;
-  // this.score = info.score;
-  // this.id = info.id;
+  this.lat = info.lat;
+  this.long = info.long;
+  this.type = info.type;
+  this.score = info.score;
+  this.id = info.id;
 };
 
+Location.all = [];
+Location.fetch = function(){
+  var url = "/locations";
+  var request = $.getJSON(url).then(function(response){
+    for(var i = 0; i < response.length; i++){
+      Location.all.push(new Location(response[i]));
+    }
+  }).fail(function(response){
+    console.log("js failed to load");
+  });
+  return request;
+};
+
+// do we need this?????
 module.exports = Location;
