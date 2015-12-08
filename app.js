d47669d9a37715f4c862925b3e4131b5fbee1b48
Markham Shofner <markham.shofner@gmail.com>
2015-12-08 16:31:41 -0500

    add routes for yelp search representation as a json object

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var locationsController = require("./controllers/locationsController");
 var seeds = require("./db/seeds");
-
+var Yelp = require("./models/yelp.js");
 // app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
@@ -13,6 +13,13 @@ app.get("/", function(req, res){
   res.render("index.html");
 });
 
+app.get("/yelp", function(req,res) {
+  // See http://www.yelp.com/developers/documentation/v2/search_api
+  Yelp.search({ term: 'bar', location: 'dc' })
+  .then(function (data) {
+    res.json(data);
+  });
+});
 
 var routes = require('./config/routes');
 app.use(routes);
