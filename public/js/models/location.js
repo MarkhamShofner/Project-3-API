ae24ba2f40fa4579923961cb002156067e49b9c2
Matt Popovich <mattpopovich@outlook.com>
2015-12-09 12:50:17 -0500

    Setting up location model to receive JSON data

--- public/js/models/location.js
+++ public/js/models/location.js
@@ -7,15 +7,50 @@ var Location = function(info){
   this.id = info.id;
 };
 
-Location.all = [];
-Location.fetch = function(){
-  var url = "/locations";
-  var request = $.getJSON(url).then(function(response){
-    for(var i = 0; i < response.length; i++){
-      Location.all.push(new Location(response[i]));
-    }
+// Location.all = [];
+// Location.fetch = function(){
+//   var url = "/locations";
+//   var request = $.getJSON(url).then(function(response){
+//     for(var i = 0; i < response.length; i++){
+//       Location.all.push(new Location(response[i]));
+//     }
+//   }).fail(function(response){
+//     console.log("js failed to load");
+//   });
+//   return request;
+// };
+
+
+Location.prototype.loadLocation = function(){
+  var self = this
+  var
+  var url = "/yelp"
+  var request = $.getJSON(url)
+  .then(function(response){
+
+    // --------- Andy's Blog Post Sample Code ------------
+
+    // self.tempF = response.current_observation.temp_f
+    // self.iconUrl = response.current_observation.icon_url
+    // self.description = response.current_observation.weather
+
+    // --------- Just pulling from first result ------------
+
+    self.name = response.businesses[0].name
+    self.category = response.businesses[0].categories[0][0]
+    self.image_url = response.businesses[0].image_url
+
+    // --- Eventually: Loop over responses and return an array of hashes? -----
+
+    // for(var i = 0; i < response.businesses.length; i++){
+    //
+    //   var self = this
+    //   self.
+    //
+    // }
+
   }).fail(function(response){
-    console.log("js failed to load");
+    console.log("Failed to load JSON.");
   });
   return request;
 };
