0eafff33f66bed06b6c29b9ee268067c098dac16
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-09 11:08:22 -0500

    functional login popup

--- public/script.js
+++ public/script.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
 	// 	console.log("you clicked");
 	// }
 
-	$('#modal').on('click', function(e){
-		e.preventDefault;
-	});
+	$('#openPopup').on('click', function(e){
+		console.log("click");
+		$("#popup").show();
+        // $("body").append(''); $("#popup").show(); 
+        // $(".close").click(function(e) { 
+        //     $(".popup, .overlay").hide(); 
+    }); 
 
 
 
