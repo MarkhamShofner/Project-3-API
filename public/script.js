5586e7e707831339b62564e20aacea9b00646719
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-09 14:46:17 -0500

    can exit login/signup prompts

--- public/script.js
+++ public/script.js
@@ -4,20 +4,23 @@ $(document).ready(function(){
 	// 	console.log("you clicked");
 	// }
 
-	$('#openLogin').on('click', function(e){
+	$('#openLogin').on('click', function(){
 		$("#signupPopup").hide();
 		$("#loginPopup").show();
 		$("#openLogin").addClass('selected');
 		$("#openSignup").removeClass('selected');
-
     }); 
 
-    $('#openSignup').on('click', function(e){
+    $('#openSignup').on('click', function(){
 		$("#loginPopup").hide();
 		$("#signupPopup").show();
 		$("#openSignup").addClass('selected');
 		$("#openLogin").removeClass('selected');
-
     }); 
+
+    $('.close').on('click', function(e){
+    	e.preventDefault();
+		$('.authPopup').hide();
+    });
 
 });
