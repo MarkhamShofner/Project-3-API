956a3bac4d2a1b2146af0e72dd8a8098bf76edc9
Markham Shofner <markham.shofner@gmail.com>
2015-12-09 13:52:24 -0500

    pull out hardcoded params into a params object that can be passed into yelp search function

--- controllers/yelpController.js
+++ controllers/yelpController.js
@@ -1,16 +1,20 @@
 var Yelp = require("../models/yelp.js");
 
 var yelpController = {
-  search: function (req, res) {
-    Yelp.search({
+
+  search: function(req, res) {
+    var params = {
       term: 'bar',
       ll: "38.999, -79.011",
       limit: 3
-    })
+    };
+
+    Yelp.search(params)
     .then(function(data) {
       res.json(data);
     });
   }
+
 };
 
 module.exports = yelpController;
