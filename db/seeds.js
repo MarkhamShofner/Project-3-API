98f6104ee9a0555abca0420f27e6b01ccb7db27f
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-10 11:27:19 -0500

    skipping console log of bars

--- db/seeds.js
+++ db/seeds.js
@@ -46,14 +46,14 @@ for(var i = 1; i < 15; i++){
     score: randInt(0, 10)
   });
   locations.push(bars[i])
-  console.log(bars[i].name + " is a " + bars[i].type + " with score " + bars[i].score);
+  // console.log(bars[i].name + " is a " + bars[i].type + " with score " + bars[i].score);
 }
 
 for (var i =0; i < locations.length; i++){
   locations[i].save(function(err){
     if (err){
       console.log(err);
-    }else {
+    } else {
       console.log( "Location " + locations[i] + " saved." );
     }
   });
