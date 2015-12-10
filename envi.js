76d98335c69db347c27710e114276b7af2c4878c
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-10 13:22:51 -0500

    fixing maybe?

--- envi.js
+++
@@ -1,9 +0,0 @@
-env = {
-    yelp_consumer_key: '2aG6U7P6vvEGp-lmjs8xrw',
-    yelp_consumer_secret: 'C-nmQ134vovjFpYg5_qtw5Kl728',
-    yelp_token: 'gCtEjHdecF8Q0WrafRtA1HHHWOJK_r9e',
-    yelp_token_secret: 'qmSvaZ0zIpGxYQKW1MQc3LfYe2s',
-    passport_secret: 'project3-mapparty'
-};
-
-module.exports = env;
