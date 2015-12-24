9744339e69d9a7aaaf9d24bbb81ab75c09e8555b
Markham Shofner <markham.shofner@gmail.com>
2015-12-24 17:56:39 -0600

    Delete env.js

--- env.js
+++
@@ -1,9 +0,0 @@
-env = {
-   yelp_consumer_key: '2aG6U7P6vvEGp-lmjs8xrw',
-   yelp_consumer_secret: 'C-nmQ134vovjFpYg5_qtw5Kl728',
-   yelp_token: 'OmUC0FW5U1HH_ah74H6_59J012a3dH-u',
-   yelp_token_secret: '6VUWPCsPbNrw8KMEDgzGVfh1QcE',
-   passport_secret: 'project3-mapparty'
-};
-
-module.exports = env;
